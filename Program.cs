a68e0d016d00472b19f96cdb66028fca61a1c301
rafaelsouzars <rafael21.santos@hotmail.com>
2023-08-20 11:23:52 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace slothhtml
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("SLOHHTML 1.0");
+            Console.WriteLine("Inicializador e gerenciador de projetos HTML5.");            
+        }
+    }
+}
