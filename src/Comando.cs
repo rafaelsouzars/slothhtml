9b5e8b223c0de5ab5e4c00672bcb19be4c4e93d9
muirak <rafael21.santos@hotmailcom>
2023-08-20 18:39:03 -0300

    Comandos básicos

---
+++ src/Comando.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace slothhtml.src
+{
+    class Comando
+    {
+        private string _nomeComando;        
+        private string _descricao;
+        private string _param;
+        private Action _funcao;
+
+        public string GetNome {
+
+            get => _nomeComando;       
+        
+        }
+        
+
+        public string GetDescricao
+        {
+
+            get => _descricao;
+
+        }
+
+        public string Param
+        {
+
+            get => _param;
+
+            set
+            {
+                _param = value;
+            }
+
+        }
+
+        public Action Funcao
+        {
+
+            get => _funcao;
+
+            set
+            {
+                _funcao = value;
+            }
+
+        }
+
+
+        public Comando(string nomeComando, string descricao, Action funcao)
+        {
+            _nomeComando = nomeComando;            
+            _descricao = descricao;
+            _funcao = funcao;
+        }
+
+        public Comando(string nomeComando, string descricao, string param, Action funcao)
+        {
+            _nomeComando = nomeComando;
+            _descricao = descricao;
+            _param = param;
+            _funcao = funcao;
+        }
+
+
+        public void Executar() {
+            _funcao();
+        }
+
+
+
+
+    }
+}
---
+++ src/ListaComandos.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace slothhtml.src
+{
+    class ListaComandos
+    {
+        private List<Comando> _comandos;
+        public ListaComandos()
+        {
+            _comandos = new List<Comando>();
+
+            _comandos.Add(new Comando("help", "Mostra a lista de comandos do SlothHtml.", () => {
+
+                Console.WriteLine("Comando help foi executado");
+
+            }));
+
+            _comandos.Add(new Comando("init", "Cria um projeto no SlothHtml.", () => {
+
+                
+                Procedimentos.Init(_comandos[1].Param);               
+                
+
+            }));
+
+            _comandos.Add(new Comando("search", "Pesquisa por bibliotecas no repositorio cdnjs.", async () => {
+
+                Procedimentos.LibsSearch(_comandos[1].Param).Wait();
+                Console.ReadKey();
+
+            }));
+
+        }
+
+        
+        public List<Comando> Comandos
+        {
+            get => _comandos;
+        }
+         
+    }
+}
