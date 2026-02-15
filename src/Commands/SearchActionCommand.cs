02d1904d1795ea8906b1bb0577393076a0cd2f68
rafaelsouzars <rafael21.santos@hotmail.com>
2026-02-15 17:04:23 -0300

    :sparkles: feat: New Command Library Description: Add prompito new command library, rename command _init_ for _create_ and a new command _create -e_ for create browser extension. Author: @rafaelsouzars

---
+++ src/Commands/SearchActionCommand.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using prompito.Prompito.Classes;
+using slothhtml.src;
+
+namespace slothhtml.src.Commands
+{
+    internal class SearchActionCommand : ActionCommand
+    {
+        public SearchActionCommand() {
+            AddFlag(
+                "-h",
+                "--help",
+                "Ajuda do comando Search"
+                );
+        }
+
+        public override void Run(ArgsMapper argsMapper)
+        {
+            //base.Run(argsMapper);
+            try
+            {
+                MappedLineTester(argsMapper, "arg1", () => {
+                    Help();
+                });
+
+                MappedLineTester(argsMapper, "arg1 arg2", () => {
+                    //Console.WriteLine("Primeiro argumento: {0}", argsMapper.GetArgs("arg1"));
+                    Procedimentos.LibsSearch(argsMapper.GetArgs("arg2")).Wait();
+                });
+
+                MappedLineTester(argsMapper, "arg1 flag1", "flag1=-h", () => {
+                    Help();
+                });
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(" [ ERROR ]\n\t{0}", e.Message);
+            }
+            
+
+        }
+    }
+}
