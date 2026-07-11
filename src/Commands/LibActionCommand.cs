836ed0c87d25f2660d911446fd9c4afc35bcf579
rafaelsouzars <rafael21.santos@hotmail.com>
2026-07-11 18:42:03 -0300

    :broom: cleanup: Using Description: Remove using. Author: @rafaelsouzars

--- src/Commands/LibActionCommand.cs
+++ src/Commands/LibActionCommand.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using prompito.Prompito.Classes;
 
 namespace slothhtml.src.Commands
